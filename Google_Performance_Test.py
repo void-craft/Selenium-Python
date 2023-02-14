890819d01694092557f73c2efcf67580cfacf5cf
Hema Priya <void-meow@users.noreply.github.com>
2023-02-14 20:36:52 +0100

    Updating Performance Test

--- Google_Performance_Test.py
+++ Google_Performance_Test.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 chrome_options = Options()
@@ -12,60 +10,53 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://www.google.com")
 
-start_time1 = time.time()
-
 reject_cookies = driver.find_element(by=By.ID, value="W0wltc")
+start_time1 = time.time()
 reject_cookies.click()
-
 cookie_time = time.time() - start_time1
 print("Cookie Rejection time: ", cookie_time)
 
-start_time2 = time.time()
-
-search_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "q")))
+# Locating search box and entering "Chennai", clicking enter and calculating time until result, taking screenshot of the tab
+search_box = driver.find_element(by=By.NAME, value="q")
 search_box.send_keys("Chennai")
+start_time2 = time.time()
 search_box.submit()
-
-wait = WebDriverWait(driver, 10)
-wait.until(EC.title_contains("Chennai"))
-
 search_time = time.time() - start_time2
 print("Search time: ", search_time)
+driver.save_screenshot("Chennai_web.png")
 
+# Opening image tab, calculating time taken, taking screenshot, closing the tab
 start_time3 = time.time()
-
 images_link = driver.find_element(by=By.LINK_TEXT, value="Imágenes")
 images_link.send_keys(Keys.CONTROL + Keys.RETURN)
 driver.switch_to.window(driver.window_handles[1])
-driver.save_screenshot("images.png")
+image_time = time.time() - start_time3
+print("Images time: ", image_time)
+driver.save_screenshot("Chennai_images.png")
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
 
-image_time = time.time() - start_time3
-print("Images time: ", image_time)
-
+# Opening news tab, calculating time taken, taking screenshot, closing the tab
 start_time4 = time.time()
-
 news_link = driver.find_element(by=By.LINK_TEXT, value="Noticias")
 news_link.send_keys(Keys.CONTROL + Keys.RETURN)
 driver.switch_to.window(driver.window_handles[1])
+news_time = time.time() - start_time4
+print("News time: ", news_time)
 driver.save_screenshot("news.png")
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
 
-news_time = time.time() - start_time4
-print("News time: ", news_time)
 
+# Opening news tab, calculating time taken, taking screenshot, closing the tab
 start_time5 = time.time()
-
 maps_link = driver.find_element(by=By.LINK_TEXT, value="Maps")
 maps_link.send_keys(Keys.CONTROL + Keys.RETURN)
 driver.switch_to.window(driver.window_handles[1])
+map_time = time.time() - start_time5
+print("Maps time: ", map_time)
 driver.save_screenshot("maps.png")
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
-
-map_time = time.time() - start_time5
-print("Maps time: ", map_time)
 
 driver.quit()
